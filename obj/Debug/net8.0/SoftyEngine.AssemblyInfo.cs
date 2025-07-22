55367872073e44db31b36044f01d2f74c403e6a0
somozadev <somozadev@gmail.com>
2025-07-22 02:31:00 +0100

    [^] Added demo gifs and build

--- obj/Debug/net8.0/SoftyEngine.AssemblyInfo.cs
+++ obj/Debug/net8.0/SoftyEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftyEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9102699f9fac31e902c23e128ec3200ea479af45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6da5c8b1d20db7bfee67a8c9118e5380c5ea6f18")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftyEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftyEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
